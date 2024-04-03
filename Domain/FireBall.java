ef5a4d91a78172c78b73111f1a130127aa22c8e4
arastudi <aarmonaite23@ku.edu.tr>
2024-04-03 23:46:24 +0300

    imlpementation of FireBall and GameController

---
+++ Domain/FireBall.java
@@ -0,0 +1,13 @@
+package Domain;
+
+public class FireBall {
+    int Score;
+    int coordX;
+    int coordY;
+    int speed;
+    int angle;
+
+    void releaseFireball() {}
+    void moveFireBall(int speed, int coordX, int coordY, int angle) {}
+    void changeFireballState(int size, String color, String logic) {}
+}
---
+++ Domain/GameController.java
@@ -0,0 +1,11 @@
+package Domain;
+
+public class GameController {
+    String currentMode;
+
+    void startTheGame() {}
+    void createTheGame() {}
+
+    void generateSpell(String spellType) {}
+    void changeGameMode(String currentMode) {}
+}
