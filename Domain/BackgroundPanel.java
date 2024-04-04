244e951ca7cee7245ca7e2295ddcc07328b11afb
gaymaz19 <gaymaz19@ku.edu.tr>
2024-04-05 02:37:01 +0300

    implemented BackgroundPanel and GameLayoutPanel

---
+++ Domain/BackgroundPanel.java
@@ -0,0 +1,29 @@
+package Domain;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+class BackgroundPanel extends JPanel {
+    private BufferedImage backgroundImage;
+
+    public BackgroundPanel() {
+        setLayout(new BorderLayout());
+        try {
+            backgroundImage = ImageIO.read(new File("/Users/gulbeyazb/lanceofdestiny/Assets/Images/200Background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
