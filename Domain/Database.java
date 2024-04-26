f6164665f78adb779cc095303b85b603737f9bd4
igorcvijanovic <ga84kiq@mytum.de>
2024-04-26 18:10:09 +0300

    worked on mongo db and some minor changes

---
+++ Domain/Database.java
@@ -0,0 +1,13 @@
+package Domain;
+
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoClients;
+
+public class Database {
+    /**private static final String URI = "mongodb+srv://<username>:<password>@<cluster-url>/<dbname>?retryWrites=true&w=majority";
+    private static final MongoClient CLIENT = MongoClients.create(URI);
+
+    public static MongoClient getClient() {
+        return CLIENT;
+    }**/
+}
--- Domain/WelcomePage.java
+++ Domain/WelcomePage.java
@@ -1,12 +1,17 @@
 package Domain;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class WelcomePage extends JFrame {
     private BuildingModeController buildingModeController;
+
+    private String backgroundImagePath = "Assets/Images/BuildingModeStartBackground.png";
 
     public WelcomePage(BuildingModeController buildingModeController) {
         this.buildingModeController = buildingModeController;
@@ -17,15 +22,14 @@ public class WelcomePage extends JFrame {
         setResizable(false);
         setLayout(new BorderLayout());
 
-        JLabel titleLabel = new JLabel("Lance of Destiny", SwingConstants.CENTER);
+        // Load and set the background image
+        setContentPane(new BuildingModeMenu.BackgroundPanel(backgroundImagePath));
+
+        JLabel titleLabel = new JLabel("Welcome to Lance of Destiny!", SwingConstants.CENTER);
+        titleLabel.setForeground(Color.white);
         titleLabel.setFont(new Font("Serif", Font.BOLD, 48));
         titleLabel.setPreferredSize(new Dimension(800, 100));
         add(titleLabel, BorderLayout.NORTH);
-
-        JLabel welcomeLabel = new JLabel("Welcome to Lance of Destiny!", SwingConstants.CENTER);
-        welcomeLabel.setFont(new Font("Serif", Font.BOLD, 24));
-        welcomeLabel.setPreferredSize(new Dimension(800, 50));
-        add(welcomeLabel, BorderLayout.CENTER);
 
         JButton loginButton = new JButton("Start");
         loginButton.addActionListener(new ActionListener() {
@@ -37,7 +41,24 @@ public class WelcomePage extends JFrame {
         });
         add(loginButton, BorderLayout.SOUTH);
     }
+    // Inner class to use a background image
+    static class BackgroundPanel extends JPanel {
+        private Image backgroundImage;
 
+        public BackgroundPanel(String imagePath) {
+            try {
+                backgroundImage = ImageIO.read(new File(imagePath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
+    }
 
     /*public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
