0ee008359e50131ff32590da42b6646f60fe4563
ealtuntas <ealtuntas19@ku.edu.tr>
2024-04-26 15:00:14 +0300

    pause screen update

---
+++ Domain/PauseScreen.java
@@ -0,0 +1,61 @@
+package Domain;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PauseScreen extends JFrame {
+    private JButton helpButton;
+    private JButton saveButton;
+    private JButton exitButton;
+    public PauseScreen() {
+        super("Pause");
+        setSize(250,150);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        helpButton = new JButton("Help");
+        exitButton = new JButton("Exit");
+        saveButton = new JButton("Save");
+
+        helpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(PauseScreen.this, "Help window will be opened", "Help", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog(PauseScreen.this, "Are you sure you want to exit?", "Confirm Exit", JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(PauseScreen.this, "Game Saved", "Save", JOptionPane.INFORMATION_MESSAGE);
+
+            }
+        });
+
+        JPanel buttonPanel = new JPanel(new GridLayout(3, 1));
+        buttonPanel.add(helpButton);
+        buttonPanel.add(exitButton);
+        buttonPanel.add(saveButton);
+        add(buttonPanel);
+
+
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            PauseScreen pauseScreen = new PauseScreen();
+            pauseScreen.setVisible(true);
+        });
+    }
+
+
+}
