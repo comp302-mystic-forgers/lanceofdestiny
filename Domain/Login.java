0b0b1689b5f1ec161a81eabacd9f203f6a95d468
igorcvijanovic <ga84kiq@mytum.de>
2024-04-27 16:32:58 +0300

    worked on database

---
+++ Domain/Login.java
@@ -0,0 +1,64 @@
+package Domain;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+    private JTextField userNameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+    private JPanel newPanel;
+    private JButton sigupButton;
+    private BuildingModeController buildingModeController;
+
+    public Login(BuildingModeController buildingModeController) {
+        this.buildingModeController = buildingModeController;
+        setTitle("Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 100);
+        setLayout(new GridLayout(3, 2));
+        JLabel userNameLabel = new JLabel("username: ");
+        add(userNameLabel);
+        userNameField = new JTextField();
+        add(userNameField);
+
+        JLabel passwordLabel = new JLabel("Password: ");
+        add(passwordLabel);
+        passwordField = new JPasswordField();
+        add(passwordField);
+
+        loginButton = new JButton("Login: ");
+        loginButton.addActionListener(this);
+        add(loginButton);
+
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginButton) {
+            String username = userNameField.getText();
+            String password = new String(passwordField.getPassword());
+            if (isValidLogin(username, password)) {
+                JOptionPane.showMessageDialog(this, "Login is successful", "Success", JOptionPane.INFORMATION_MESSAGE);
+                buildingModeController.setCurrentMode("building_mode_menu");
+                buildingModeController.switchScreens();
+                this.dispose();
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid username or password. Please try again.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private boolean isValidLogin(String username, String password) {
+        return username.length() > 5 && password.length() > 6;
+    }
+
+   /**
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            BuildingModeController buildingModeController = new BuildingModeController();
+            Login login = new Login(buildingModeController);
+        });
+    }**/
+
+}
--- src/main/java/Domain/FireBall.java
+++ src/main/java/Domain/FireBall.java
@@ -1,24 +1,24 @@
-package src.main.java.Domain;
+package Domain;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 public class FireBall {
-    private double x, y; // Fireball position
+    private int x, y; // Fireball position
     private final int diameter = 20; // Fireball size
-    double xVelocity = 3; // Fireball horizontal movement speed
-    double yVelocity = 2; // Fireball vertical movement speed
+    private int xVelocity = 1; // Fireball horizontal movement speed
+    private int yVelocity = - 1; // Fireball vertical movement speed
     private ImageIcon icon;
 
     public boolean isBallActive = false;
 
-    public FireBall(double startX, double startY) {
+    public FireBall(int startX, int startY) {
         this.x = startX;
         this.y = startY;
         this.icon = new ImageIcon("Assets/Images/200FireBall.png");
     }
-    public void setVelocity(double xVel, double yVel) {
+    public void setVelocity(int xVel, int yVel) {
         this.xVelocity = xVel;
         this.yVelocity = yVel;
     }
@@ -64,10 +64,10 @@ public class FireBall {
 
     public void draw(Graphics g) {
         if (icon!= null) {
-            g.drawImage(icon.getImage(), (int) x, (int) y, diameter, diameter, null);
+            g.drawImage(icon.getImage(), x, y, diameter, diameter, null);
         } else {
             g.setColor(Color.RED);
-            g.fillOval((int) x, (int) y, diameter, diameter);
+            g.fillOval(x, y, diameter, diameter);
         }
     }
 
@@ -84,7 +84,7 @@ public class FireBall {
         xVelocity *= -1;
     }
     // Getter methods
-    public double getX() { return x; }
-    public double getY() { return y; }
+    public int getX() { return x; }
+    public int getY() { return y; }
     public int getDiameter() { return diameter; }
 }
