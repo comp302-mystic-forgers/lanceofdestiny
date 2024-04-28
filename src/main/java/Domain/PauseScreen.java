f13a593e9a91690f27b2a41d813effa5d3d16f55
ealtuntas <ealtuntas19@ku.edu.tr>
2024-04-29 00:43:41 +0300

    Revert "pause screen update"
    This reverts commit ce0d36b3

--- src/main/java/Domain/PauseScreen.java
+++ src/main/java/Domain/PauseScreen.java
@@ -5,15 +5,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-
 public class PauseScreen extends JFrame {
     private JButton helpButton;
     private JButton saveButton;
     private JButton exitButton;
-    public PauseScreen(GameWindow gameWindow) {
+    public PauseScreen() {
         super("Pause");
-
         setSize(250,150);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         helpButton = new JButton("Help");
@@ -53,16 +50,12 @@ public class PauseScreen extends JFrame {
 
         setLocationRelativeTo(null);
     }
-
-    public  void closePauseScreen() {
-        this.dispose(); //
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            PauseScreen pauseScreen = new PauseScreen();
+            pauseScreen.setVisible(true);
+        });
     }
-   // public static void main(String[] args) {
-       // SwingUtilities.invokeLater(() -> {
-        //    PauseScreen pauseScreen = new PauseScreen();
-        //    pauseScreen.setVisible(true);
-        //});
-   // }
 
 
 }
