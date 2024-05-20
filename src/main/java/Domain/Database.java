d586c671a789f78f9426a5c28e4fe6de3ae36bbb
gaymaz19 <gaymaz19@ku.edu.tr>
2024-05-20 16:32:50 +0300

    refactored the Database class's constructor

--- src/main/java/Domain/Database.java
+++ src/main/java/Domain/Database.java
@@ -7,19 +7,22 @@ import org.bson.UuidRepresentation;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public class Database {
-    private static final String CONNECTION_STRING = "mongodb+srv://gaymaz19:YkGu8iPQz2oj7MfZ@cluster0.teltnbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+    private static final String DEFAULT_CONNECTION_STRING = "mongodb+srv://gaymaz19:YkGu8iPQz2oj7MfZ@cluster0.teltnbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     public MongoClient mongoClient;
     public MongoDatabase database;
-    ConnectionString connString = new ConnectionString(CONNECTION_STRING);
-    MongoClientSettings settings = MongoClientSettings.builder()
-            .applyConnectionString(connString)
-            .uuidRepresentation(UuidRepresentation.STANDARD)
-            .build();
+    public MongoClientSettings settings;
 
     public Database() {
+        this(DEFAULT_CONNECTION_STRING);
+    }
+
+    public Database(String connectionString) {
+        ConnectionString connString = new ConnectionString(connectionString);
+        settings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .build();
         mongoClient = MongoClients.create(settings);
         database = mongoClient.getDatabase("LanceOfDesitny");
     }
