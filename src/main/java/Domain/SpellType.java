c1d07327e5de72e112755abc8a422d0c1041bdfd
fozen19 <fozen19@ku.edu.tr>
2024-05-30 22:18:52 +0300

    New SpellTypes are added.

--- src/main/java/Domain/SpellType.java
+++ src/main/java/Domain/SpellType.java
@@ -4,8 +4,10 @@ public enum SpellType {
     FELIX_FELICIS("Felix Felicis"),
     MAGICAL_STAFF_EXPANSION("Magical Staff Expansion"),
     HEX("Hex"),
-    OVERWHELMING_FIRE_BALL("Overwhelming Fire Ball");
-
+    OVERWHELMING_FIRE_BALL("Overwhelming Fire Ball"),
+    INFINITE_VOID("Infinite Void"),
+    DOUBLE_ACCELERATION("Double Acceleration"),
+    HOLLOW_PURPLE("Hollow Purple");
     private final String name;
 
     SpellType(String name) {
